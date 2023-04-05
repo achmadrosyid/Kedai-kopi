e41bb224531ecde57dd993b4f35e52eaa84cccaa
stynore <setiyono.ressly@gmail.com>
2023-04-05 19:23:09 +0700

    update master blade

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.master')
+@section('content-header')
+<div class="row mb-2">
+    <div class="col-sm-6">
+        <h1 class="m-0">Categori</h1>
+    </div><!-- /.col -->
+    <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+            <li class="breadcrumb-item"><a href="{{url('home')}}">Home</a></li>
+            <li class="breadcrumb-item active">Categori</li>
+        </ol>
+    </div><!-- /.col -->
+</div><!-
+@endsection
