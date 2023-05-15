aac83276786092422cfc235c59acecfd203953da
achmadrosyid <siboystorytelling01@gmail.com>
2023-05-15 18:27:10 +0700

    pop up cashier

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -3,17 +3,43 @@
 <section class="buttom-header">
   <div class="container fluid">
     <div class="inner. col-lg-12">
-          <button type="button" class="extrasmall. btn-blok btn-primary btn-md col-3">Add Category</button>
+          <button type="button" class="extrasmall. btn-blok btn-primary btn-md col-3" data-toggle="modal" data-target="#modal-detail">Add Cashier</button>
     </div>
   </div>
 </section>
+<div class="modal" id="modal-detail" tabindex="-1" role="dialog">
+<div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title">Modal title</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+      <div class="form-group">
+        <label for="input-username">Username</label>
+        <input type="username" class="form-control" id="input-username" placeholder="Input username">
+        <label for="input-password">password</label>
+        <input type="password" class="form-control" id="input-password" placeholder="Input password">
+        <label for="input-No. Hp">No. Hp</label>
+        <input type="No. Hp" class="form-control" id="input-No. Hp" placeholder="Input No. Hp">
+      </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
 <br>
  <section class="content">
     <div class="container fluid">
       <div class="col-lg-12">
         <div class="card">
           <div class="card-header">
-            <h3 class="card-title">Category </h3>
+            <h3 class="card-title"> Cashier </h3>
           </div>
             <div class="card-body">
                  <div id="example1_wrapper" class="dataTables_wrapper dt-bootstrap4">
