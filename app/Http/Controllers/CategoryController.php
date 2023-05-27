40bd4ffbb6f7cea943e2b33056afaf76eb643bfb
stynore <setiyono.ressly@gmail.com>
2023-05-27 20:27:26 +0700

    addd datable category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -4,13 +4,27 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Category;
+use Yajra\DataTables\DataTables;
 
 class CategoryController extends Controller
 {
-    public function index() {
-        $category = Category::query()
+    public function index(Request $request) {
+        $data = Category::query()
         ->select('id','nama')
         ->get();
+        if ($request->ajax()){
+            return DataTables::of($data)
+                ->addColumn('nama',function ($row){
+                    return $row->nama;
+                })
+                ->addColumn('action',function ($row){
+                    return
+                        ' <a href="javascript:void(0)"  class="btn btn-success btn-sm"  id="my-btn-edit" data-id="'.$row->id.'" data-toggle="tooltip" data-placement="top" title="Edit this record"><i class="fa fa-edit"></i> Edit</a>
+                    <a href="javascript:void(0)" class="btn btn-danger btn-sm" id="my-btn-delele" data-id="'.$row->id.'" ><i class="fa fa-trash"></i> Delete</a> ';
+                })
+                ->rawColumns(['nama','action'])
+                ->make(true);
+        }
         return view('category.index');
     }
 
--- public/functionjs/category.js
+++ public/functionjs/category.js
@@ -1,3 +1,26 @@
+var baseurl = $('#url').val();
+var token = $('#token').val();
+$(document).ready(function(){
+    getData();
+});
+function getData() {
+    $('#data-table').DataTable({
+        paging      : true,
+        searching   : false,
+        info        : true,
+        ordering    : true,
+        bDestroy    : true,
+        ajax: {
+            url: '/category',
+            type: "GET",
+        },
+        columns:[
+            { data: 'nama'},
+            { data: 'nama'},
+            { data: 'action',width: "15%"}
+        ],
+    });
+}
 // add modal
 function create(){
    $('#modalCreate').modal('show');
