6d8f8eb9713ad1a284004708dfbbca763abe4cbd
stynore <setiyono.ressly@gmail.com>
2023-05-28 09:46:52 +0700

    addd no in category

--- public/functionjs/category.js
+++ public/functionjs/category.js
@@ -1,27 +1,35 @@
 var baseurl = $('#url').val();
 var token = $('#token').val();
-$(document).ready(function(){
+$(document).ready(function () {
     getData();
 });
+
 function getData() {
     $('#data-table').DataTable({
-        paging      : true,
-        searching   : false,
-        info        : true,
-        ordering    : true,
-        bDestroy    : true,
+        paging: true,
+        searching: false,
+        info: true,
+        ordering: true,
+        bDestroy: true,
         ajax: {
             url: '/category',
             type: "GET",
+            dataSrc: function (json) {
+                json.data.forEach(function (row, index) {
+                    row.no = index + 1; // Menambahkan nomor data secara otomatis
+                });
+                return json.data;
+            }
         },
-        columns:[
-            { data: 'nama'},
-            { data: 'nama'},
-            { data: 'action',width: "15%"}
+        columns: [
+            {data: 'no'},
+            {data: 'nama'},
+            {data: 'action', width: "15%"}
         ],
     });
 }
+
 // add modal
-function create(){
-   $('#modalCreate').modal('show');
+function create() {
+    $('#modalCreate').modal('show');
 }
