a38ea5524a0875c78fa9c5a7c5167f7baaf784ae
achmadrosyid <siboystorytelling01@gmail.com>
2023-06-04 20:18:47 +0700

    migration

---
+++ database/migrations/2023_06_04_131508_create_products_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('id_category');
+            $table->text('img');
+            $table->string('nama');
+            $table->text('description');
+            $table->tinyInteger('status')->default(0);
+            $table->integer('harga');
+            $table->timestamps();
+            $table->foreign('id_category')->references('id')->on('category');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product');
+    }
+}
