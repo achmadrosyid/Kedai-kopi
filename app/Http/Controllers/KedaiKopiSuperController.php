79c0325696f5685af30bfacccca23839a2df061f
achmadrosyid <siboystorytelling01@gmail.com>
2023-07-08 17:59:16 +0700

    fix home customer view

--- app/Http/Controllers/KedaiKopiSuperController.php
+++ app/Http/Controllers/KedaiKopiSuperController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
+use Illuminate\Support\Facades\Validator;
+use Yajra\DataTables\DataTables;
 
 class KedaiKopiSuperController extends Controller
 {
-    public function index() {
-        return view('kedai-kopi-super.index');
+    public function index(Request $request) {
+        $data = Product::query()
+            ->select('id','nama','img', 'harga')
+            ->where('status',1)
+            ->get();
+        return view('kedai-kopi-super.index',compact('data'));
     }
 }
